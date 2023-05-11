10ec19d5ca0adc2f6edb65a10defe96ad9f0111a
Bellmere <therise00@gmail.com>
2023-05-11 18:38:55 +0300

    change styles

--- src/components/biodigital/iframe-human/IFrameHuman.jsx
+++ src/components/biodigital/iframe-human/IFrameHuman.jsx
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 const paramsIframe = [
   'ui-info=false',
@@ -10,23 +10,32 @@ const paramsIframe = [
   'ui-reset=false',
   'dk=' + process.env.REACT_APP_BIO_PUBLIC_KEY,
 ];
-export default function IFrameHuman({ scene, init = () => {}, className = '', children }) {
-  const idModel = scene?.length > 10 ? `m=${scene}.json` : `be=${scene}`
+export default function IFrameHuman({
+  scene,
+  init = () => {},
+  className = '',
+  children,
+}) {
+  const idModel = scene?.length > 10 ? `m=${scene}.json` : `be=${scene}`;
   return (
-    <div style={{position: 'relative', height: '100%', width: '100%'}}>
-      <div style={{position: 'absolute', right: '20px', top: '20px'}}>
+    <div
+      className="loh"
+      style={{ position: 'relative', height: '100%', width: '100%' }}
+    >
+      <div style={{ position: 'absolute', right: '20px', top: '20px' }}>
         {children}
       </div>
       <iframe
-        id='myWidget'
+        id="myWidget"
         title="myWidget"
         onLoad={init}
         className={`${className} iframe-human`}
-        src={`https://human.biodigital.com/widget/?${paramsIframe.join('&')}&${idModel}`}
-        width='100%'
+        src={`https://human.biodigital.com/widget/?${paramsIframe.join(
+          '&'
+        )}&${idModel}`}
+        width="100%"
         height="100%"
-      >
-      </iframe>
+      ></iframe>
     </div>
   );
 }
